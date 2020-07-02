f3aa28ddea20e01cde768b089e357fb18e205544
Emile Bergez <emilebergez@gmail.com>
2020-07-02 17:47:12 +0200

    tried to fix

--- src/Contexts/ProfileContext.js
+++ src/Contexts/ProfileContext.js
@@ -3,26 +3,21 @@ import React, { useState, createContext } from "react";
 export const ProfileContext = createContext();
 
 const ProfileContextProvider = (props) => {
+  const [posts, setPosts] = useState([{ rating: 1 }]);
 
-  const [posts, setPosts] = useState([]);
-  
-    fetch("http://localhost:8000/apip/posts/351")
-      .then((response) => response.json())
-      .then(
-        (data) =>  { 
-        setPosts(data.note);
-       
-        },
-        
-        console.log(posts)
-      )      
-      
-      const rating = posts.note;
-    return (
-      <ProfileContext.Provider value={ rating }>
-        {props.children}
-      </ProfileContext.Provider>
+  fetch("http://localhost:8000/apip/posts/351")
+    .then((response) => response.json())
+    .then(
+      (data) => {
+        console.log(data);
+      },
+
+      console.log(posts.rating)
     );
-
+  return (
+    <ProfileContext.Provider value={posts}>
+      {props.children}
+    </ProfileContext.Provider>
+  );
 };
 export default ProfileContextProvider;
