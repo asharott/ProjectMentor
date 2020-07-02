6efe9ee3e0f629dc366c52571d05fc7507603500
Emile Bergez <emilebergez@gmail.com>
2020-07-02 19:39:23 +0200

    added page 1

---
+++ src/components/NextButton/NextButton.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { ProgressContext } from "../../contexts/ProgressContext";
+
+const NextButton = (props) => {
+  const { increasePercentage } = useContext(ProgressContext);
+  return (
+    <div className="nextButtonContainer">
+      <button className="nextButton" onClick={() => increasePercentage()}>
+        Continuer
+      </button>
+    </div>
+  );
+};
+
+export default NextButton;
--- src/components/Page1/Page1.js
+++ src/components/Page1/Page1.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+import NextButton from "../NextButton/NextButton";
 
 const Page1 = (props) => {
   return (
     <div>
       <div className="jobSearchSection">
         <h2>Quel est votre métier ?</h2>
-        <input type="search" className="jobSearchBar" placeholder="Recherchez votre métier"></input>
+        <input
+          type="search"
+          className="jobSearchBar"
+          placeholder="Rentrez votre métier."
+        ></input>
       </div>
+      <NextButton />
     </div>
   );
 };
