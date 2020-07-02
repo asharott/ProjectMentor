ee868f101b6b588a5741e4370a519be5e86defcf
Quentin <quentin.monteferrario@gmail.com>
2020-07-02 17:16:34 +0200

    request ajax rating

--- src/Components/Testfetch.js
+++ src/Components/Testfetch.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+
 const Testfetch = (props) => {
-  fetch("http://localhost:8000/api/post")
+  fetch("http://localhost:8000/apip/posts/351")
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data.lastName));
   return "";
 };
+
 
 export default Testfetch;
--- src/Contexts/ProfileContext.js
+++ src/Contexts/ProfileContext.js
@@ -3,12 +3,26 @@ import React, { useState, createContext } from "react";
 export const ProfileContext = createContext();
 
 const ProfileContextProvider = (props) => {
-  const [rating] = useState(5);
-  return (
-    <ProfileContext.Provider value={{ rating }}>
-      {props.children}
-    </ProfileContext.Provider>
-  );
-};
 
+  const [posts, setPosts] = useState([]);
+  
+    fetch("http://localhost:8000/apip/posts/351")
+      .then((response) => response.json())
+      .then(
+        (data) =>  { 
+        setPosts(data.note);
+       
+        },
+        
+        console.log(posts)
+      )      
+      
+      const rating = posts.note;
+    return (
+      <ProfileContext.Provider value={ rating }>
+        {props.children}
+      </ProfileContext.Provider>
+    );
+
+};
 export default ProfileContextProvider;
