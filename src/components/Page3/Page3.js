9b643ed143ff732b8bf18cafe55a1043532e8546
Emile Bergez <emilebergez@gmail.com>
2020-07-04 02:08:16 +0200

    fixed problems with svg

--- src/components/Page3/Page3.js
+++ src/components/Page3/Page3.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import NextButton from "../NextButton/NextButton";
 import { ProgressContext } from "../../contexts/ProgressContext";
-import { ReactComponent as MinusTwo } from "/Assets/2-.svg";
-import { ReactComponent as TwotoFive } from "/Assets/2to5.svg";
-import { ReactComponent as FiveToTen } from "/Assets/5to10.svg";
-import { ReactComponent as Plus10 } from "/Assets/10+";
+import { ReactComponent as MinusTwo } from "./Assets/Two.svg";
+import { ReactComponent as TwotoFive } from "./Assets/TwoToFive.svg";
+import { ReactComponent as FiveToTen } from "./Assets/FiveToTen.svg";
+import { ReactComponent as Plus10 } from "./Assets/Ten.svg";
 
 const Page3 = (props) => {
   const { job } = useContext(ProgressContext);
@@ -12,12 +12,20 @@ const Page3 = (props) => {
     <div>
       <div className="pageThreeContent">
         <h2>Depuis combien de temps êtes vous {job} ?</h2>
-        <div className="experienceContainer">
-          <MinusTwo />
-          <TwotoFive />
-          <FiveToTen />
-          <Plus10 />
-        </div>
+        <ul className="experienceContainer">
+          <li>
+            <MinusTwo />
+          </li>
+          <li>
+            <TwotoFive />
+          </li>
+          <li>
+            <FiveToTen />
+          </li>
+          <li>
+            <Plus10 />
+          </li>
+        </ul>
       </div>
       <NextButton />
     </div>
