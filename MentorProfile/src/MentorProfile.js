31d2b30a43f5dedb89334440d5217c8c5a4e690a
Emile Bergez <emilebergez@gmail.com>
2020-07-08 12:43:16 +0200

    before switching files

---
+++ MentorProfile/src/MentorProfile.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Styles.scss";
+import HeaderMentor from "./components/HeaderMentor/HeaderMentor";
+import CoverPicture from "./components/CoverPicture/CoverPicture";
+import Infos from "./components/Infos/Infos";
+import Skills from "./components/Skills/Skills";
+import Resume from "./components/Resume/Resume";
+import MentoringType from "./components/MentoringType/MentoringType";
+import MeetingType from "./components/MeetingType/MeetingType";
+import Experience from "./components/Experience/Experience";
+import Opinion from "./components/Opinions/Opinions";
+import Contact from "./components/Contact/Contact";
+import ProfileContextProvider from "./Contexts/ProfileContext";
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileContextProvider>
+        <HeaderMentor />
+        <CoverPicture />
+        <Infos />
+        <Skills />
+        <Resume />
+        <MentoringType />
+        <MeetingType />
+        <Experience />
+        <Opinion />
+        <Contact />
+      </ProfileContextProvider>
+    </div>
+  );
+}
+
+export default App;
